42f1f6c9914aa9e25f062e5c283da936e9051c1f
KMSFriedrichDerGrosse <1160542372@qq.com>
2025-12-02 14:36:54 +0800

    update dataset info

--- sparql_ply/__init__.py
+++
@@ -1,5 +0,0 @@
-from sparql_ply.sparql_yacc import parse as parse_sparql
-from sparql_ply.components import (
-    NodeTerm, PropertyPath, CollectionPath, BlankNodePath, TriplesPath,
-    Expression, GraphPattern, Query, ComponentWrapper, QueryComponent,
-)
